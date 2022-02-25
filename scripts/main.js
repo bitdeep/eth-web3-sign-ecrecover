145934f984417b9a2eebe1ca194072e2975be303
bitdeep <19378023+bitdeep@users.noreply.github.com>
2022-02-24 23:14:33 -0300

    clean deploy script sample code

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,10 @@
 const hre = require("hardhat");
 // npx hardhat run scripts\mainnet.js --network cro
 async function main() {
-    const mintPass = '0x844dacd5a52db9368e6c606f739599598031da84';
-    const old = '0xfc1255755829c460D938e76f7Fa6de04505925e1';
-    const _CSS = await hre.ethers.getContractFactory("NFT2");
-    const CSS = await _CSS.deploy(mintPass, old);
-    await CSS.deployed();
+    const _main = await hre.ethers.getContractFactory("main");
+    const main = await _main.deploy(mintPass, old);
+    await main.deployed();
     console.log("contract:", CSS.address);
-    await CSS.setBaseURI('https://398dofhadsf.cronossociety.com/meta/');
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
